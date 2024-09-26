65514d50f8fc174d6b6198ecb0f29447a0058a1c
Joey Pierre <JoeyPierre@joeypierre27@gmail.com>
2024-09-26 16:29:43 -0400

    98% done

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get("/", async (req, res) => {
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
+
 // server.js
 
 // POST /fruitsd
@@ -41,6 +42,11 @@ app.get("/fruits", async (req, res) => {
   console.log(allFruits);
   res.render("fruits/index.ejs", { fruits: allFruits });
 });
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
